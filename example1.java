d38588abaee3cb5eb81aef66b6825b3ccd9c90aa
Kayeon <kayeonalvina@gmail.com>
2022-10-21 19:28:42 -0400

    example 1 done

---
+++ example1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class example1 {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+
+        int input;
+        input = in.nextInt();
+
+        System.out.println(input);
+
+        in.close();
+
+    }
+}
---
+++ example1.py
@@ -0,0 +1,11 @@
+val = input("what is the number?")
+
+nums = val.split(" ")
+
+val = int(nums[0])
+val2 = int(nums[1])
+print(val + val2)
+print(val-val2)
+print(val*val2)
+print(val//val2)
+print(val%val2)

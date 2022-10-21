c22fae0faa71eea19a5a87301f6f0c8eb23e3dec
Kayeon <kayeonalvina@gmail.com>
2022-10-21 17:31:08 -0400

    test

---
+++ test.java
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+public class test {
+
+    public static void main(String[] args) {
+        
+        int a = 3;
+
+        if (a != 4) {
+            System.out.println(a+1);
+        }
+    }
+}

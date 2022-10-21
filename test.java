2b46b384bf9586ff3da33144c6dc53801d38a83d
haminhwang <hhwan2@ocdsb.ca>
2022-10-21 18:21:04 -0400

    Test 1

--- test.java
+++ test.java
@@ -6,9 +6,12 @@ public class test {
     public static void main(String[] args) {
         
         int a = 3;
+        int b = 4;
 
         if (a != 4) {
             System.out.println(a+1);
         }
+
+        System.out.println(b-2);
     }
 }
